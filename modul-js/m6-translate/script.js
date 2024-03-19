2c670aebad1034b12c635d1b2cb01a2458335a00
hanbit10 <chanbit10@gmail.com>
2024-03-19 09:15:39 +0100

    JS kurs update

---
+++ modul-js/m6-translate/script.js
@@ -0,0 +1,10 @@
+function english() {
+  document.getElementById("headline").innerHTML = "Hello";
+  document.getElementById("text").innerHTML = "Welcome on my awesome page!";
+}
+
+function deutsch() {
+  document.getElementById("headline").innerHTML = "Hallo";
+  document.getElementById("text").innerHTML =
+    "Wilkommen auf meiner tollen Seite!";
+}
