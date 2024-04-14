de21d40ac64ed903a076d639c147ae62286669b3
hanbit10 <chanbit10@gmail.com>
2024-04-14 14:01:11 +0200

    circle bar

--- modul8-prompt-engineering/fotogalerie/script.js
+++ modul8-prompt-engineering/fotogalerie/script.js
@@ -1,19 +1,19 @@
 let images = [
   "./img/1.jpg",
-  "./img/2.jpg",
-  "./img/3.jpg",
-  "./img/4.jpg",
-  "./img/5.jpg",
-  "./img/6.jpg",
-  "./img/7.jpg",
-  "./img/8.jpg",
-  "./img/9.jpg",
-  "./img/10.jpg",
-  "./img/11.jpg",
-  "./img/12.jpg",
-  "./img/13.jpg",
-  "./img/14.jpg",
-  "./img/15.jpg",
+  // "./img/2.jpg",
+  // "./img/3.jpg",
+  // "./img/4.jpg",
+  // "./img/5.jpg",
+  // "./img/6.jpg",
+  // "./img/7.jpg",
+  // "./img/8.jpg",
+  // "./img/9.jpg",
+  // "./img/10.jpg",
+  // "./img/11.jpg",
+  // "./img/12.jpg",
+  // "./img/13.jpg",
+  // "./img/14.jpg",
+  // "./img/15.jpg",
 ];
 
 // Function to initialize and load images
@@ -38,4 +38,18 @@ function init() {
     // Append the div to the container
     container.appendChild(div);
   });
+
+  setProgress(100);
 }
+
+function setProgress(percentage) {
+  const progressBar = document.getElementById("progress-bar");
+  const circumference = 2 * Math.PI * 40;
+  const offset = circumference - (percentage / 100) * circumference;
+  progressBar.setAttribute("stroke-dashoffset", offset);
+}
+
+// Example: Change the percentage after 2 seconds
+setTimeout(() => {
+  setProgress(50); // Change the percentage to 50%
+}, 2000);
