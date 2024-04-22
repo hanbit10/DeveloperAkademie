f76f4919c75d9e4324e0e33e777509b45892f411
hanbit10 <chanbit10@gmail.com>
2024-04-22 15:53:50 +0200

    jsdoc

---
+++ modul10/jsdoc/script.js
@@ -0,0 +1,8 @@
+/**
+ * This function is used to greet someone on the command line
+ *
+ * @param {string} name - This is the name of the person that you want to greet
+ */
+function greet(name) {
+  console("Hallo" + name);
+}
