658e66c3acc713c407d71403badde04dd25da5b9
hanbit10 <chanbit10@gmail.com>
2024-06-08 19:38:59 +0200

    Klassen

--- modul12-el-pollo-loco/objektorientierung_vorkurs/script.js
+++ modul12-el-pollo-loco/objektorientierung_vorkurs/script.js
@@ -1,9 +1,18 @@
 let contacts = [
   {
     'firstName': 'Max',
-    'lastName': 'Mustermann',
+   'surName': 'Mustermann',
+   'address': 'Alexianergraben 40, 52062 Aachen',	
   }
 ];
+
+class Contact {
+  constructor(firstName, surName, address){
+    this.firstName = firstName
+    this.surName = surName
+    this.address = address
+  }
+}
 
 function addContact(firstName, lastName){
   contacts.push({
